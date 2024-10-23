8343832bd77259051d32d3c5f8043e23a61ea4d0
shopna's Macbook <sharminshopna149@gmail.com>
2024-10-23 11:37:42 +0600

    add button in the country card

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,8 +1,14 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
     console.log(country)
     const {name, flags,population,area,cca3} = country
+    const[visited,setVisited]=useState(false)
+    const handleVisited=()=>{
+        // setVisited(true)
+        setVisited(!visited)
+    }
     return (
         <div className="country">
             <img src={flags.png} alt="" />
@@ -10,7 +16,8 @@ const Country = ({country}) => {
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p><small>Code: {cca3}</small></p>
-            <button>Visited</button>
+            <button onClick={handleVisited}>{visited ?'Visited':'Going'}</button>
+            {visited ? 'I have visited this country' : 'I want to visit'}
         </div>
     );
 };
